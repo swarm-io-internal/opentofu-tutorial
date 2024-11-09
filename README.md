a00a2a34e7cc02454c963cdb17a8bd7b3064c2f9
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-11-09 14:50:23 +0000

    chore (infra): Update instance.tf

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# opentofu-tutorial
+# OpenTofu Tutorial
+
+## Start Using Codespaces
+
+[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/swarm-io-internal/opentofu-tutorial?quickstart=1)
+
+## Setup AWS CLI
+Initiate a Codespace from the OpenTofu Tutorial repository and, once ready, run `aws configure` in the terminal to input your AWS credentials.
+
+### Resources
+[How to Retrieve AWS Secret Keys](https://www.msp360.com/resources/blog/how-to-find-your-aws-access-key-id-and-secret-access-key/)
+
+## Setup OpenTofu
+To verify your OpenTofu installation, run `tofu --version`
+
+## Resources
+[Terraform Tutorial](https://developer.hashicorp.com/terraform/tutorials)
+
+[Terraform Registry](https://library.tf)
---
+++ infra/instance.tf
@@ -0,0 +1,29 @@
+# Specify the AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.47.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2" # Oregon servers
+}
+
+# Create a t2.micro EC2 instance
+resource "aws_instance" "tutorial_instance" {
+  ami           = "ami-0b72821e2f351e396" # Amazon Machine Image (AMI); https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/AMIs.html
+  instance_type = "t2.micro" # EC2 Instance; https://aws.amazon.com/ec2/instance-types/
+
+  tags = {
+    Name = "<myname>-tofu" # Change to your name, so we can see all the instances online
+  }
+}
+
+# Manage the instance state (e.g., start, stop)
+resource "aws_ec2_instance_state" "tutorial_instance_state" {
+  instance_id = aws_instance.tutorial_instance.id
+  state       = "running" # Set to "stopped" if you want to stop the instance instead of running it
+}
